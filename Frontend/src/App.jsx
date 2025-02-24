4df96eba4d570a34d9c34b686effd31046e14c65
yarin5445 <yarin5445@gmail.com>
2025-02-24 10:59:14 +0200

    Add Footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import Title from './components/shared/Title';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import HomePage from './pages/Homepage';
+import Footer from './components/shared/Footer';
 
 function App() {
-   const [count, setCount] = useState(0);
-
    return (
-      <>
-         <div>
-            <Title title="Homepage" />
-            <a href="https://vite.dev" target="_blank">
-               <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://react.dev" target="_blank">
-               <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
+      <BrowserRouter>
+         <div className="d-flex flex-column side-allPage">
+            {/* <Header/> */}
+            <main>
+               <Container className="mt-3">
+                  <Routes>
+                     <Route path="/" element={<HomePage />} />
+                  </Routes>
+               </Container>
+            </main>
+            <Footer />
          </div>
-         <h1>Vite + React</h1>
-         <div className="card">
-            <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-            <p>
-               Edit <code>src/App.jsx</code> and save to test HMR
-            </p>
-         </div>
-         <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-      </>
+      </BrowserRouter>
    );
 }
-
 export default App;
