26bc3e9b68920eeed9efbce549cfcdf89987d911
yarin5445 <yarin5445@gmail.com>
2025-03-03 10:47:35 +0200

    Add MessageBox Component

---
+++ Frontend/src/components/MessageBox.jsx
@@ -0,0 +1,16 @@
+import Alert from 'react-bootstrap/Alert';
+import PropTypes from 'prop-types';
+
+const MessageBox = ({ children, variant = 'info' }) => {
+  return <Alert variant={variant}>
+    {children}
+  </Alert>;
+};
+
+
+MessageBox.propTypes = {
+  children: PropTypes.node,
+  variant: PropTypes.string,
+};
+
+export default MessageBox;
