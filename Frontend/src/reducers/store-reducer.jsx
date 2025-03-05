5e1d3cc71a7aec80f108b8116e51486747b8ab57
yarin <yarin5445@gmail.com>
2025-03-05 15:45:07 +0200

    Add addToCart Action to storeReducer

--- Frontend/src/reducers/store-reducer.jsx
+++ Frontend/src/reducers/store-reducer.jsx
@@ -1,11 +1,27 @@
-import { USER_SIGNIN, USER_SIGNOUT } from '../actions.js';
+import { ADD_TO_CART, USER_SIGNIN, USER_SIGNOUT } from '../actions.js';
 
 const storeReducer = (state, { type, payload }) => {
   switch (type) {
     case USER_SIGNIN:
       return { ...state, userInfo: payload };
     case USER_SIGNOUT:
-      return { ...state, userInfo: null };
+      localStorage.clear();
+      return {
+        userInfo: null,
+        cart: { cartItems: [], shippingAddress: {}, paymentMethod: '' },
+      };
+    case ADD_TO_CART: {
+      const newItem = payload;
+      const existingItems = state.cart.cartItems.find(item => item.token === newItem.token);
+
+      const cartItems = existingItems
+        ? state.cart.cartItems.map(item => (item.token === existingItems.token ? newItem : item))
+        : [...state.cart.cartItems, newItem];
+
+      localStorage.setItem('cartItems', JSON.stringify(cartItems));
+
+      return { ...state, cart: { ...state.cart, cartItems } };
+    }
     default:
       return state;
   }
