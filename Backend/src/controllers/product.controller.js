2f49e29c47e7749d3c7470ea4e7f3962b55be485
yarin <yarin5445@gmail.com>
2025-03-17 11:58:58 +0200

    Add Product Search Route

--- Backend/src/controllers/product.controller.js
+++ Backend/src/controllers/product.controller.js
@@ -31,3 +31,45 @@ export const getProductCategories = async (req, res, next) => {
     return next(generateCustomError(500, `Server Error: ${error.message}`));
   }
 };
+
+export const getProductsByQuery = async (req, res, next) => {
+  try {
+    const { query } = req;
+    const pageSize = 10;
+
+    const page = query.page || 1;
+    const category = query.category || '';
+    const price = query.price || '';
+    const minPrice = Number(price.split('-'))[0];
+    const maxPrice = Number(price.split('-'))[1];
+    const rating = query.rating || '';
+    const order = query.order || '';
+    const searchQuery = query.query || '';
+
+    const categoryFilter = category && category !== 'all' ? { category } : {};
+    const priceFilter =
+      price && price !== 'all' ? { price: { $gte: minPrice, $lte: maxPrice } } : {};
+    const ratingFilter =
+      rating && rating !== 'all' ? { 'rating.rate': { $gte: Number(rating) } } : {};
+    // 1 for ascending, -1 for descending
+    const sortOrder =
+      order === 'lowest'
+        ? { price: 1 }
+        : order === 'highest'
+        ? { price: -1 }
+        : order === 'topRated'
+        ? { rating: -1 }
+        : { _id: -1 };
+    const queryFilter =
+      searchQuery && searchQuery !== 'all' ? { title: { $regex: searchQuery, $options: 'i' } } : {};
+    const filter = { ...queryFilter, ...categoryFilter, ...ratingFilter, ...priceFilter };
+    const products = await Product.find(filter)
+      .sort(sortOrder)
+      .skip(pageSize * (page - 1))
+      .limit(pageSize);
+    const countProducts = await Product.countDocuments(filter);
+    res.send({ products, countProducts, pages: Math.ceil(countProducts / pageSize) });
+  } catch (error) {
+    return next(generateCustomError(500, `Server Error: ${error.message}`));
+  }
+};
--- Backend/src/routers/product.router.js
+++ Backend/src/routers/product.router.js
@@ -3,12 +3,14 @@ import {
   getProducts,
   getProductByToken,
   getProductCategories,
+  getProductsByQuery,
 } from '../controllers/product.controller.js';
 
 const productRouter = Router();
 
 productRouter.get('/', getProducts);
-productRouter.get('/token/:token', getProductByToken);
+productRouter.get('/search', getProductsByQuery);
 productRouter.get('/categories', getProductCategories);
+productRouter.get('/token/:token', getProductByToken);
 
 export default productRouter;
