ead26ec9f0bcc1b679ea387969f10ae91a024bee
yarin <yarin5445@gmail.com>
2025-03-17 10:25:43 +0200

    Add Order Page, Add Logic To Finish SearchBox Component

--- Frontend/src/components/shared/search-box.jsx
+++ Frontend/src/components/shared/search-box.jsx
@@ -2,20 +2,36 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { getFilterUrl } from '../../utils';
 
 const SearchBox = () => {
   const [query, setQuery] = useState('');
   const navigate = useNavigate();
   const { search } = useLocation();
+  const navigateByQuery = () => {
+    if (query) {
+      const filterUri = getFilterUrl(search, { query });
+      navigate(filterUri);
+    }
+  };
+
+  // useEffect(() => {
+  //   navigateByQuery();
+  // }, [navigateByQuery, query]);
+
+  const submitHandler = e => {
+    e.preventDefault();
+    navigateByQuery();
+  };
   return (
-    <Form onSubmit={() => {}} className="d-flex me-auto w-50">
+    <Form onSubmit={submitHandler} className="d-flex me-auto w-50">
       <InputGroup>
         <FormControl
           type="text"
           placeholder="Search for products..."
-          onChange={() => {}}
+          onChange={e => setQuery(e.target.value)}
         ></FormControl>
         <Button variant="warning" type="submit">
           <i className="fas fa-search"></i>
--- Frontend/src/pages/order.page.jsx
+++ Frontend/src/pages/order.page.jsx
@@ -1,5 +1,170 @@
+import { useContext } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import useRequest from '../hooks/use-request';
+import { Store } from '../store';
+import Loading from '../components/shared/loading';
+import MessageBox from '../components/shared/message-box';
+import Title from '../components/shared/title';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 const OrderPage = () => {
-  return <div></div>;
+  const { state } = useContext(Store);
+  const { userInfo } = state;
+  const params = useParams();
+  const { id: orderId } = params;
+  const navigate = useNavigate();
+  const {
+    isLoading,
+    error,
+    data: order,
+  } = useRequest(`/api/v1/orders/${orderId}`, {
+    headers: {
+      authorization: `Bearer ${userInfo?.token}`,
+    },
+  });
+  if (!userInfo) {
+    navigate('/signin');
+    return null;
+  }
+  return (
+    <div>
+      {' '}
+      {isLoading ? (
+        <Loading />
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div>
+          {' '}
+          <Title title="Order" />{' '}
+          <h1 className="my-3"> Order: {order._id.substr(order._id.length - 6)} </h1>{' '}
+          <Row>
+            {' '}
+            <Col md={8}>
+              {' '}
+              <Card className="mb-3">
+                {' '}
+                <Card.Body>
+                  {' '}
+                  <Card.Title>Shipping</Card.Title>{' '}
+                  <Card.Text>
+                    {' '}
+                    <strong>Name:</strong> {order.shippingAddress.fullName} <br />{' '}
+                    <strong>Address:</strong> {order.shippingAddress.address},{' '}
+                    {order.shippingAddress.city}, {order.shippingAddress.country},{' '}
+                    {order.shippingAddress.postalCode}{' '}
+                  </Card.Text>{' '}
+                  {order.isDelivered ? (
+                    <MessageBox variant="success"> Delivered at {order.deliveredAt} </MessageBox>
+                  ) : (
+                    <MessageBox variant="danger">Not Delivered</MessageBox>
+                  )}{' '}
+                </Card.Body>{' '}
+              </Card>{' '}
+              <Card className="mb-3">
+                {' '}
+                <Card.Body>
+                  {' '}
+                  <Card.Title>Payment</Card.Title>{' '}
+                  <Card.Text>
+                    {' '}
+                    <strong>Method:</strong> {order.paymentMethod}{' '}
+                  </Card.Text>{' '}
+                  {order.isPaid ? (
+                    <MessageBox variant="success"> Paid at {order.paidAt} </MessageBox>
+                  ) : (
+                    <MessageBox variant="danger">Not Paid</MessageBox>
+                  )}{' '}
+                </Card.Body>{' '}
+              </Card>{' '}
+              <Card className="mb-3">
+                {' '}
+                <Card.Body>
+                  {' '}
+                  <Card.Title>Items</Card.Title>{' '}
+                  <ul>
+                    {' '}
+                    {order.orderItems.map(item => (
+                      <li key={item._id}>
+                        {' '}
+                        <Row className="align-items-center">
+                          {' '}
+                          <Col md={6}>
+                            {' '}
+                            <img
+                              src={item.image}
+                              alt={item.name}
+                              className="img-fluid rounded img-thumbnail"
+                            ></img>{' '}
+                            <Link to={`/product/${item.token}`}> {item.title} </Link>{' '}
+                          </Col>{' '}
+                          <Col md={3}>
+                            {' '}
+                            <span>{item.quantity}</span>{' '}
+                          </Col>{' '}
+                          <Col md={3}>${item.price}</Col>{' '}
+                        </Row>{' '}
+                      </li>
+                    ))}{' '}
+                  </ul>{' '}
+                </Card.Body>{' '}
+              </Card>{' '}
+            </Col>{' '}
+            <Col md={4}>
+              {' '}
+              <Card className="mb-3">
+                {' '}
+                <Card.Body>
+                  {' '}
+                  <Card.Title>Order Summary</Card.Title>{' '}
+                  <ul>
+                    {' '}
+                    <li>
+                      {' '}
+                      <Row>
+                        {' '}
+                        <Col>Items</Col> <Col>${order.itemsPrice.toFixed(2)}</Col>{' '}
+                      </Row>{' '}
+                    </li>{' '}
+                    <li>
+                      {' '}
+                      <Row>
+                        {' '}
+                        <Col>Shipping</Col> <Col>${order.shippingPrice.toFixed(2)}</Col>{' '}
+                      </Row>{' '}
+                    </li>{' '}
+                    <li>
+                      {' '}
+                      <Row>
+                        {' '}
+                        <Col>Tax</Col> <Col>${order.taxPrice.toFixed(2)}</Col>{' '}
+                      </Row>{' '}
+                    </li>{' '}
+                    <li>
+                      {' '}
+                      <Row>
+                        {' '}
+                        <Col>
+                          {' '}
+                          <strong>Order Total</strong>{' '}
+                        </Col>{' '}
+                        <Col>
+                          {' '}
+                          <strong>${order.totalPrice.toFixed(2)}</strong>{' '}
+                        </Col>{' '}
+                      </Row>{' '}
+                    </li>{' '}
+                  </ul>{' '}
+                </Card.Body>{' '}
+              </Card>{' '}
+            </Col>{' '}
+          </Row>{' '}
+        </div>
+      )}{' '}
+    </div>
+  );
 };
 
 export default OrderPage;
